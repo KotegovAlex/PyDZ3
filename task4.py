a8b8205f608b4986c8551a2eecb93bbd40710971
Alexander Kotegov <s.kotegov2010@yandex.ru>
2022-09-12 12:18:53 +0500

    Added task4

---
+++ task4.py
@@ -0,0 +1,31 @@
+# 4. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+    # Пример:
+    # 45 -> 101101
+    # 3 -> 11
+    # 2 -> 10
+
+# Вариант 1:
+
+# bin_number = bin(int(input('Введите десятичное число -> ')))
+# print(bin_number[2:])
+
+# # Вариант 2:
+
+import string
+
+
+dec_number = int(input('Введите десятичное число -> '))
+
+def dec_to_bin(dec_num:int):
+    temp = dec_num
+    result = ''
+    while temp > 2:
+        result = result + str(temp % 2)
+        temp = temp // 2
+    else:
+        result = result + '1'
+
+    result = result[::-1]
+    return result
+
+print(f'Десятичное {dec_number} равно двоичному {dec_to_bin(dec_number)}')
